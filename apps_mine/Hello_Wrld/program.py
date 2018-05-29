2066ea432385c8c03cf75b1d87fba2485f6870e2
CastilloK <Kevin.Castillo@ngc.com>
2018-05-29 16:59:28 -0400

    Created a git ignore and did hello test project

---
+++ apps_mine/Hello_Wrld/program.py
@@ -0,0 +1,7 @@
+print('HELLO APP')
+
+# Ctrl + Q for documentation
+# red file names mean file not checked into git
+
+user_text = input('what is your name? ')
+print('Hey there ' + user_text + '!')
